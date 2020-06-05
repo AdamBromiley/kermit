a14f7ef179a259319ad4f143bc89a63013f025e5
Adam Bromiley <adam@bromiley.co.uk>
2020-06-05 18:33:43 +0100

    Create wrapper for Caesar Cipher encryptor

---
+++ basics/classical-ciphers/caesar-cipher/include/caesar.h
@@ -0,0 +1,27 @@
+#ifndef CAESAR_H
+#define CAESAR_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <limits.h>
+
+#include <parser.h>
+#include <caesar_cipher.h>
+
+
+#define OPTSTRING ":des:"
+
+#define SHIFT_DEFAULT 3
+#define SHIFT_MIN INT_MIN
+#define SHIFT_MAX INT_MAX
+
+#define READ_BUFFER_SIZE 1024
+#define OUTPUT_BUFFER_SIZE READ_BUFFER_SIZE
+
+
+int ioHandler(FILE *inputStream, FILE *outputStream, int encryptionShift, enum Mode encryptionMode);
+
+
+#endif
